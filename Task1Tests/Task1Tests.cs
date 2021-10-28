58e4bd0ca57bed2ef4d11d91efb456097be737f7
Arnas <arnas.vaiceliunas@gmail.com>
2021-10-28 14:13:09 +0300

    Cleaning unnecessary tests

--- Task1Tests/Task1Tests.cs
+++ Task1Tests/Task1Tests.cs
@@ -15,22 +15,11 @@ namespace Task1Tests
             var passParameter1 = 5;
             long result1;
 
-            var passParameter2 = 7;
-            long result2;
-
-            var passParameter3 = 8;
-            long result3;
-
             //Act
             result1 = await Calculator.CalculateAsync(passParameter1, CancellationToken.None);
-            result2 = await Calculator.CalculateAsync(passParameter2, CancellationToken.None);
-            result3 = await Calculator.CalculateAsync(passParameter3, CancellationToken.None);
 
             //Assert
             Assert.AreEqual(result1, 15);
-            Assert.AreEqual(result2, 28);
-            Assert.AreEqual(result3, 36);
-
         }
 
         [TestMethod]
